6dfda27839fdbf59a8e0c0e16354f68893afc337
Eduardo-Decarli <decarliedu@gmail.com>
2025-10-29 20:37:04 -0300

    fix: the routerLink to Transactions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { Index } from './componentes/index';
 import { General } from './componentes/general/general';
+import { Transactions } from './componentes/transactions/transactions';
 
 export const routes: Routes = [
   {
     path: '',
-    component: Index
+    component: Index,
   },
   {
     path: 'general',
-    component: General
-  }
+    component: General,
+  },
+  {
+    path: 'transactions',
+    component: Transactions,
+  },
 ];
