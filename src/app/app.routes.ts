1726cb1ccdef7c9591be9203702644310963906c
Eduardo-Decarli <decarliedu@gmail.com>
2025-10-29 20:31:51 -0300

    feat: create index, general and transactions section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Index } from './componentes/index';
+import { General } from './componentes/general/general';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Index
+  },
+  {
+    path: 'general',
+    component: General
+  }
+];
